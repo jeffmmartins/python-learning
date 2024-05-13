1bf71138f026469c804a22a772caebae1c95e303
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-05-13 18:44:06 -0300

    exemplo string

---
+++ string.py
@@ -0,0 +1,16 @@
+nome = "Jefferson"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = "  ola mundo  "
+
+print(texto.strip())
+print(texto.rstrip())
+print(texto.lstrip())
+
+menu = "Python"
+
+print(menu.center(14, "#"))
+print("-".join(menu))
