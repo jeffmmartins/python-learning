eb544d718c5238e1340e603566e577e142236ba6
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-05-20 11:59:56 -0300

    exemplo string tripla

---
+++ string_triplas.py
@@ -0,0 +1,20 @@
+nome = "Guilherme"
+
+mensagem = f"""
+    olá meu nome é {nome}
+Eu Estou aprendendo Python
+    Essa mensagem tem diferentes recuos
+"""
+
+print(mensagem)
+
+print("""
+      ====================== MENU =====================
+      
+      1 - Depositar 
+      2 - Sacar
+      3 - Sair
+
+      =================================================
+                Obrigado por usar nosso Sistemas!
+    """)
